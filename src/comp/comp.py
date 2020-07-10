27e375e3eca58a00c998011ba934211cbaaecd0c
toricruzmendiola <mtoricruz@gmail.com>
2020-07-10 10:58:41 -0700

    toricruz-mendiola

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -61,9 +61,10 @@ print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
-# The "humans" list should be unmodified.
 print("All names uppercase:")
-g = [(i.name.upper(), i.age+5) for i in humans]
+# The "humans" list should be unmodified. My original condition was modding list.
+# This conditional modifies the Human instance itself
+g = [Human((i.name).upper(), (i.age + 5)) for i in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
